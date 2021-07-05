d7b31214298ed1bcb9c194ebc28dd1996bb947b4
BezaT123 <bezat.berhe@gmail.com>
2021-07-05 14:02:13 +0300

    Add Header

---
+++ DEVDOCS.md
@@ -0,0 +1,18 @@
+# Tech used in FB clone build
+
+## React + React Hooks
+## React context API -> REDUX pattern
+## Material UI
+## FLEXBOX
+## Firebase's Firestore realtime DB
+## Firebase Google Authentication
+## React flip move
+
+
+### Todo
+#   [] clone the app
+#   [] check and add stuff to it
+#   [] header: add media-query
+
+
+
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDi8fSGeMkuCMyvSTIDAo33Ar_Lj3bzKa0",
+    authDomain: "facebook-clone-82c30.firebaseapp.com",
+    projectId: "facebook-clone-82c30",
+    storageBucket: "facebook-clone-82c30.appspot.com",
+    messagingSenderId: "723740765877",
+    appId: "1:723740765877:web:c9b0e1db1e4b820e0ed8fc"
+};
